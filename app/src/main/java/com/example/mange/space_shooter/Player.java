01030beb6ea860a789c43a22f85e9b805ac122b6
Miguel Angel Nunez <manunez7@miners.utep.edu>
2018-04-14 17:18:15 -0600

    Added the PLayer Class that Exrends Character

---
+++ app/src/main/java/com/example/mange/space_shooter/Player.java
@@ -0,0 +1,19 @@
+package com.example.mange.space_shooter;
+
+public class Player extends Character {
+
+
+
+
+
+    //Actions enemy/player can perform
+    /**Method to fire projectiles and move down**/
+    @Override
+    public void action(){}
+    /**Method to draw out the enemy/player**/
+    @Override
+    public boolean draw(){return true;}
+    /**Method for enemy passing away*/
+    @Override
+    public void onDeath(){}
+}
