eb576cf6d1c7faa143c26d2c34d850c06a4ae92b
Miguel Angel Nunez <manunez7@miners.utep.edu>
2018-05-06 15:19:20 -0600

    Implemented the SensorManager to display player ship, responds to phone tilt using revised accelorometer

--- app/src/main/java/com/example/mange/space_shooter/MainActivity.java
+++ app/src/main/java/com/example/mange/space_shooter/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.mange.space_shooter;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -8,6 +11,7 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.Gravity;
 import android.widget.Toast;
 
@@ -20,62 +24,90 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Space customView;
     private BoardConfiguration board;
 
+    private float gravity;
+    private float linear_acceleration;
+    private float currentstate;
+
+
+    public static float xPos, xAccel, xVel = 0.0f;
+    public static float yPos, yAccel, yVel = 0.0f;
+    private float xMax, yMax;
+    public static Bitmap ship;
+    private SensorManager sensorManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        customView = findViewById(R.id.area);
-        activateSensors();
-       init();
-    }
-    private void init(){
-        board = customView.board;
+//        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        Space playerShip = new Space(this);
+        setContentView(playerShip);
+
+        Point size = new Point();
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+        xMax = (float) size.x - 100;
+        yMax = (float) size.y - 100;
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        senseManagement.registerListener(this, sense, SensorManager.SENSOR_DELAY_NORMAL);
+    protected void onStart() {
+        super.onStart();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        senseManagement.unregisterListener(this);
+    protected void onStop() {
+        sensorManager.unregisterListener(this);
+        super.onStop();
     }
 
-    private void activateSensors(){
-        senseManagement = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sense = senseManagement.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        playerState = 0;
-    }
+
 
     @Override
     public final void onAccuracyChanged(Sensor sensor, int accuracy) {
         //No calibration Needed
     }
 
-
     @Override
-    public void onSensorChanged(SensorEvent event){
-        if(event.values[0] > 0.8) playerState = 1;
-        else if(event.values[0]  < -0.8 ) playerState = 2;
-        else    playerState = 0;
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            xAccel = sensorEvent.values[0];
+            yAccel = -sensorEvent.values[1];
+            updateShip();
+        }
     }
 
+    private void updateShip() {
+        float frameTime = 0.666f;
+        xVel += (xAccel * frameTime);
+        yVel += (yAccel * frameTime);
+
+        float xS = (xVel / 2) * frameTime;
+        float yS = (yVel / 2) * frameTime;
+
+        xPos -= xS;
+        yPos -= yS;
+
+        if (xPos > xMax) {
+            xPos = xMax;
+        } else if (xPos < 0) {
+            xPos = 0;
+        }
+
+        if (yPos > yMax) {
+            yPos = yMax;
+        } else if (yPos < 0) {
+            yPos = 0;
+        }
+    }
     private void toast(String msg) {
         Toast toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.TOP, 0, 170);
         toast.show();
     }
 
-    public class playerThread extends Thread{
-        @Override
-        public void run() {
-            try{sleep(1000);}
-            catch(InterruptedException e){toast("Player has become unresponsive");}
-            //add method for player to move
-        }
-    }
+
 }
--- app/src/main/java/com/example/mange/space_shooter/Space.java
+++ app/src/main/java/com/example/mange/space_shooter/Space.java
@@ -2,6 +2,8 @@ package com.example.mange.space_shooter;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
@@ -15,39 +17,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by alpha on 4/19/18.
+ * Created by Miguel on 5/6/18.
  */
 
 public class Space extends View {
-    private final List<SelectionListener> listeners = new ArrayList<>();
-    private final Paint boardPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    public BoardConfiguration board = new BoardConfiguration();
 
-    public Space(Context context, AttributeSet attrs) { //@cons
-        this(context, attrs, 0);
+
+    public Space(Context context) {
+        super(context);
+        Bitmap ballSrc = BitmapFactory.decodeResource(getResources(), R.drawable.playership);
+        final int dstWidth = 100;
+        final int dstHeight = 100;
+        MainActivity.ship = Bitmap.createScaledBitmap(ballSrc, dstWidth, dstHeight, true);
     }
 
-    /**
-     * Create a new instance by inflating it from XML and apply a class-specific base
-     * style from a theme attribute.
-     */
-    public Space(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        setSaveEnabled(true);
-        //getViewTreeObserver().addOnGlobalLayoutListener(layoutListener);
-    }
-
-    public interface SelectionListener {
-
-        /**
-         * Called when a square of the board is selected by tapping.
-         *
-         * @param x 0-based column index of the selected square.
-         * @param y 0-based row index of the selected square.
-         */
-        void onSelection(int x, int y);
-    }
     @Override
-    protected void onDraw(Canvas canvas) {}
-
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(MainActivity.ship, MainActivity.xPos, MainActivity.yPos, null);
+        invalidate();
+        }
     }
